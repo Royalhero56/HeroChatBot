27a362585a9b273a4f49198e12c99bac6456405d
Royalhero56 <158155141+Royalhero56@users.noreply.github.com>
2025-11-17 22:25:48 +0530

    Update __init__.py

--- Mickey/database/__init__.py
+++ Mickey/database/__init__.py
@@ -1,10 +1,10 @@
 from pymongo import MongoClient
-
 import config
 
+# MongoDB Connection
 vickdb = MongoClient(config.MONGO_URL)
 vick = vickdb["VickDb"]["Vick"]
 
-
+# Import internal modules
 from .chats import *
 from .users import *
